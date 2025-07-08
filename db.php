3c5068fcf866737375a25f5e4a0b1b5d53fa2ffd
brian <briankipkirui023@gmail.com>
2025-07-08 09:13:26 +0300

    Remove leading whitespace in db.php to fix session_start issue

--- db.php
+++ db.php
@@ -1,11 +1,7 @@
-
-
 <?php
-// Start session if not already started
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-
 // Load .env
 require_once __DIR__ . '/vendor/autoload.php';
 
