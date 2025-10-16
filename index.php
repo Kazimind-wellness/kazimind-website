97ca05c9711f5526b95f5d358320879277302d58
Steven Njoroge <stevenmacharia2003@gmail.com>
2025-10-16 15:59:00 +0300

    aligned the links

--- index.php
+++ index.php
@@ -146,7 +146,7 @@ Our work is rooted in cultivating resilience, clarity, and balance guiding indiv
       <li><span>Stress Management</span></li>
       <li><span>Trauma, PTSD and C-PTSD</span></li>
       <li><span>Gender and Sexuality</span></li>
-      <li><span><a href="eatingDisorder.php" style="text-decoration: underline;">Eating Disorders</a></span></li>
+      <li><span><a href="eatingDisorder.php">Eating Disorders</a></span></li>
       <li><span>Body Image</span></li>
       <!-- <li><span>Corporate Mental Health Talks</span></li> -->
     </ul>
@@ -166,7 +166,7 @@ Our work is rooted in cultivating resilience, clarity, and balance guiding indiv
       <li><span>Grief and Loss</span></li>
       <li><span>Prenatal Health and Post-Partum Support</span></li>
       <li><span>Substance Use and Recovery</span></li>
-      <li><span><a href="attachmentstyles.php" style="text-decoration: underline;">Attachment Disorders / Attachment styles</a></span></li>
+      <li><span><a href="attachmentstyles.php">Attachment Disorders / Attachment styles</a></span></li>
       <li><span>Mind-Body Connection</span></li>
       <!-- <li><span>Couples Therapy</span></li> -->
       <!-- <li><span>Family Therapy</span></li> -->
